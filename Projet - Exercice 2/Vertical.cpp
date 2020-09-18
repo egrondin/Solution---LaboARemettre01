6d0b2232ad75a9d729031b044a1d1f75aba6d7f3
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-18 11:14:58 -0400

    Bon travail
    Manque juste un test et quelques commentaires !

--- Projet - Exercice 2/Vertical.cpp
+++ Projet - Exercice 2/Vertical.cpp
@@ -1,5 +1,5 @@
-// But: Demander à l'utilisateur de taper un nombre à cinq chiffres et déterminer à l'aide du programme si ce nombre est bien à cinq chiffre et de ensuite l'afficher à la vertical
-// Auteur: Émile Grondin
+// But: Demander Ã  l'utilisateur de taper un nombre Ã  cinq chiffres et dÃ©terminer Ã  l'aide du programme si ce nombre est bien Ã  cinq chiffre et de ensuite l'afficher Ã  la vertical
+// Auteur: Ã‰mile Grondin
 // Date: 13/09/2020
 
 // Pour communiquer avec l'utilisateur
@@ -10,33 +10,37 @@ void main()
 	//Pour afficher correctement les accents dans la console
 	setlocale(LC_ALL, "");
 
-	// Déclaration de la variable
+	// DÃ©claration de la variable
 	int nombre;
 
-	// Le programme affiche un message pour demander à l'utilisateur d'entrer un nombre à cinq chiffres
-	std::cout << "Veuillez rentrer un nombre à cinq chiffres: ";
+	// Le programme affiche un message pour demander Ã  l'utilisateur d'entrer un nombre Ã  cinq chiffres
+	std::cout << "Veuillez rentrer un nombre Ã  cinq chiffres: ";
 
-	// Le programme lit le nombre qui a été tapé par l'utilisateur
+	// Le programme lit le nombre qui a Ã©tÃ© tapÃ© par l'utilisateur
 	std::cin >> nombre;
 	
-	// Le programme s'assure que le nombre entré par l'utilisateur est un nombre de cinq chiffres ou non
+	// Le programme s'assure que le nombre entrÃ© par l'utilisateur est un nombre de cinq chiffres ou non
 	if (nombre < 10000)
 	{
-		std::cout << "Ce nombre ne contient pas 5 chiffres."; // Le programme affiche que le nombre entré par l'utilisateur n'est pas un nombre à cinq chiffres
+		std::cout << "Ce nombre ne contient pas 5 chiffres."; // Le programme affiche que le nombre entrÃ© par l'utilisateur n'est pas un nombre Ã  cinq chiffres
 	}
 	else
 	{
-		std::cout << nombre / 10000 << std::endl << nombre % 10000 / 1000 << std::endl << nombre % 1000 / 100 << std::endl << nombre % 100 / 10 << std::endl << nombre % 10; // Le programme affiche le nombre entré par l'utilisateur à la verticale
+		// KM : Il faut une autre condition ici pour que le nombr n'ait pas plus de 5 chiffres
+		
+		// KM : Manque de commentaire pour expliquer les calculs
+		std::cout << nombre / 10000 << std::endl << nombre % 10000 / 1000 << std::endl << nombre % 1000 / 100 << std::endl << nombre % 100 / 10 << std::endl << nombre % 10; // Le programme affiche le nombre entrÃ© par l'utilisateur Ã  la verticale
 	}
 
 
 }
 
 // Plan de tests
+// KM : TRÃ¨s bien
 /*
 
 
-nombre:								résultat escompté:
+nombre:								rÃ©sultat escomptÃ©:
 
 12345								1
 									2
